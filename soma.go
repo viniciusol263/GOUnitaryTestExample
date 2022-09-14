57d4cea0487253d322bbe1c3ba9799d79a961dc4
viniciusol263 <viniciuscme2@gmail.com>
2022-09-14 15:23:02 -0300

    Basic Go "Testing" module example

---
+++ soma.go
@@ -0,0 +1,40 @@
+package soma
+
+import (
+	"errors"
+	"log"
+)
+
+const listSize = 5
+
+var (
+	ErrListFull      = errors.New("list is full")
+	ErrValueNotFound = errors.New("Value not found")
+)
+
+type Value struct {
+	id    int
+	value int
+}
+type Contains struct {
+	values []Value
+}
+
+func (c *Contains) insertInt(id int, value int) ([]Value, error) {
+	if len(c.values) == listSize {
+		return c.values, ErrListFull
+	}
+	c.values = append(c.values, Value{id, value})
+	log.Printf("value %d was added into the list\n", value)
+	return c.values, nil
+}
+
+func (c *Contains) searchId(id int) (*Value, error) {
+	for _, value := range c.values {
+		if value.id == id {
+			log.Printf("Found Value %+v\n", value)
+			return &value, nil
+		}
+	}
+	return &Value{}, ErrValueNotFound
+}
---
+++ soma_test.go
@@ -0,0 +1,52 @@
+package soma
+
+import (
+	"reflect"
+	"testing"
+)
+
+var containsTest Contains
+
+func TestContainer(t *testing.T) {
+	// Tests InsertInt method
+	testDataInsertInt := []struct {
+		value_t  Value
+		response []Value
+	}{
+		{value_t: Value{id: 0, value: 42}, response: []Value{{id: 0, value: 42}}},
+		{value_t: Value{id: 1, value: 231}, response: []Value{{id: 0, value: 42}, {id: 1, value: 231}}},
+		{value_t: Value{id: 2, value: 3}, response: []Value{{id: 0, value: 42}, {id: 1, value: 231}, {id: 2, value: 3}}},
+		{value_t: Value{id: 3, value: 1034}, response: []Value{{id: 0, value: 42}, {id: 1, value: 231}, {id: 2, value: 3}, {id: 3, value: 1034}}},
+		{value_t: Value{id: 4, value: 598}, response: []Value{{id: 0, value: 42}, {id: 1, value: 231}, {id: 2, value: 3}, {id: 3, value: 1034}, {id: 4, value: 598}}},
+		{value_t: Value{id: 5, value: 3423}, response: []Value{{id: 0, value: 42}, {id: 1, value: 231}, {id: 2, value: 3}, {id: 3, value: 1034}, {id: 4, value: 598}}},
+	}
+	{
+		for _, testValue := range testDataInsertInt {
+			tmpTest, err := containsTest.insertInt(testValue.value_t.id, testValue.value_t.value)
+			if !reflect.DeepEqual(tmpTest, testValue.response) {
+				t.Fatalf("Received return %v - Expected return %v, error %v\n", tmpTest, testValue.response, err)
+			}
+		}
+	}
+
+	//Tests SearchId method
+	testDataSearchId := []struct {
+		id_t     int
+		response *Value
+	}{
+		{id_t: 0, response: &Value{id: 0, value: 42}},
+		{id_t: 1, response: &Value{id: 1, value: 231}},
+		{id_t: 2, response: &Value{id: 2, value: 3}},
+		{id_t: 3, response: &Value{id: 3, value: 1034}},
+		{id_t: 4, response: &Value{id: 4, value: 598}},
+		{id_t: 5, response: &Value{}},
+	}
+	{
+		for _, testValue := range testDataSearchId {
+			tmpTest, _ := containsTest.searchId(testValue.id_t)
+			if !reflect.DeepEqual(testValue.response, tmpTest) {
+				t.Fatalf("Received return %v - Expected return %v\n", tmpTest, testValue.response)
+			}
+		}
+	}
+}
